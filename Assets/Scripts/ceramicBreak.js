b938fc4041aac00636a1799f3ca080ee4c38939e
Jessica Kwok <jk3405@nyu.edu>
2017-05-18 22:10:45 -0700

    Initial commit
    - include asset of mug in whole and broken form
    - set up prefab of mug so that it has the proper colliders and whatnot
    - first draft of script for shattering

---
+++ Assets/Scripts/ceramicBreak.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var brokenMug : GameObject;
+private var dropHeight : float;
+
+function Start () {
+	dropHeight = transform.position.y; //initial height for now
+}
+
+function Update () {
+}
+
+function OnCollisionEnter(collision: Collision) {
+	Debug.Log(dropHeight + ", " + transform.position.y);
+	if (dropHeight - transform.position.y > 1) {
+		Instantiate(brokenMug, transform.position, transform.rotation);
+    	Destroy(gameObject);
+    }
+}
